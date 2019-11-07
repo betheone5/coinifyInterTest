2fbc025b5bba1f56053258ff27f13414e4ed2a64
Rakesh Moturi <rakesh_bov@yahoo.com>
2019-11-07 20:26:22 +0100

    insuffient balacee

--- service/DefaultService.js
+++ service/DefaultService.js
@@ -1,6 +1,6 @@
 'use strict';
 const db = require("../models");
-var counts =  [];
+var counts = [];
 
 /**
  * Retrieve all currencies
@@ -8,8 +8,8 @@ var counts =  [];
  *
  * returns List
  **/
-exports.getCurrencies = function() {
-  return new Promise(function(resolve, reject) {
+exports.getCurrencies = function () {
+  return new Promise(function (resolve, reject) {
     var examples = {};
     Currency.findAll().then(curr => {
       console.log("testtt");
@@ -20,8 +20,8 @@ exports.getCurrencies = function() {
         resolve(examples[Object.keys(examples)[0]]);
       } else {
         resolve();
-    }
-});
+      }
+    });
   });
 }
 
@@ -32,19 +32,13 @@ exports.getCurrencies = function() {
  *
  * returns user
  **/
-exports.getUser = function() {
-  return new Promise(function(resolve, reject) {
+exports.getUser = function () {
+  return new Promise(function (resolve, reject) {
     var examples = {};
-//     examples['application/json'] = {
-//   "balance" : 6,
-//   "name" : "name",
-//   "id" : 0
-// };
-    User.findOne({where: {name:"Rakesh"}}).then( uuser => {
-      
+    User.findOne({ where: { name: "Rakesh" } }).then(uuser => {
+
       console.log("test");
       console.log(uuser);
-
       examples[0] = uuser;
 
       if (Object.keys(examples).length > 0) {
@@ -52,9 +46,7 @@ exports.getUser = function() {
       } else {
         resolve();
       }
-    } );
-    
-
+    });
   });
 }
 
@@ -66,24 +58,23 @@ exports.getUser = function() {
  * amount Integer amount to withdraw
  * returns withdraw
  **/
-exports.withdrawAmount = function(amount) {
-  return new Promise(function(resolve, reject) {
+exports.withdrawAmount = function (amount) {
+  return new Promise(function (resolve, reject) {
     var realAmount = amount
-    ;
-    
+      ;
+
     //Find the least combination
     //var pcounts = findLeast(amount);
-    counts= [];
+    counts = [];
 
     Currency.findAll().then(curr => {
 
-      var notes = new Array(1000, 500, 200, 100, 50, 20, 10, 5, 2, 1);
       var noteCounter = Array(10).fill(0);
-      
+
       // count notes using Greedy approach
       var i = 0;
       curr.forEach(element => {
-  
+
         //console.log(element.value)
         if (amount >= element.value) {
           noteCounter[i] = Math.floor(amount / element.value);
@@ -98,60 +89,30 @@ exports.withdrawAmount = function(amount) {
         }
         i++;
       });
-      if(counts != null){
-        
-        User.findOne({where: {name:"Rakesh"}}).then( uuser => {
+      if (counts != null) {
+
+        User.findOne({ where: { name: "Rakesh" } }).then(uuser => {
           console.log(amount);
           var newamount = uuser.balance - realAmount;
           console.log(newamount);
-          
+
           if (newamount >= 0) {
-              User.update(
-                { balance: newamount  },
-                { where:{ id: 1 }}                
-                )
-                .then(function(rowsUpdated) {
-                  console.log(rowsUpdated)
-                })
-                .catch(error => {
-                  console.log(error);
-                })
+            User.update(
+              { balance: newamount },
+              { where: { id: 1 } }
+            )
+              .then(function (rowsUpdated) {
+                console.log(rowsUpdated)
+                resolve(counts);
+              })
+              .catch(error => {
+                console.log(error);
+              })
           } else {
-            resolve("Do not have sufficient money");
+            resolve("You do not have sufficient money");
           }
-        } );
-        
-        resolve(counts);
+        });
       }
-  });
-});
-}
-
-function findLeast(amount) {
-  console.log("in find least " + amount);
-
-  Currency.findAll().then(curr => {
-
-    var notes = new Array(1000, 500, 200, 100, 50, 20, 10, 5, 2, 1);
-    var noteCounter = Array(10).fill(0);
-    
-    // count notes using Greedy approach
-    var i = 0;
-    curr.forEach(element => {
-
-      //console.log(element.value)
-      if (amount >= element.value) {
-        noteCounter[i] = Math.floor(amount / element.value);
-        //console.log("\t"+noteCounter[i]);
-        let item = {};
-        item["count"] = noteCounter[i];
-        item["id"] = element.id;
-        item["value"] = element.value;
-        counts.push(item);
-        amount = amount - noteCounter[i] * element.value;
-        //console.log("\t"+amount);
-      }
-      i++;
     });
   });
 }
